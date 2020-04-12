6cde42c572a0c9647db07f9334d976627b809a9c
David Smejkal <xsmejk10@node.mendelu.cz>
2020-04-12 18:30:18 +0200

    - implemented booking - refactored resolver to each resolver has own file

---
+++ graphgl/resolver/booking.js
@@ -0,0 +1,33 @@
+const Booking = require('../../models/booking');
+const Event = require('../../models/event');
+const { transformBooking, transformEvent } = require('./merge');
+
+module.exports = {
+  bookings: async () => {
+    try {
+      const bookings = await Booking.find();
+      return bookings.map(booking => transformBooking(booking));
+    } catch (e) {
+      throw e;
+    }
+  },
+  bookEvent: async (args) => {
+    const fetchedEvent = await Event.findOne({_id: args.eventId});
+    const booking = new Booking({
+      user: '5e8f15fde56f610b107602d3',
+      event: fetchedEvent
+    });
+    const result = await booking.save();
+    return transformBooking(result);
+  },
+  cancelBooking: async args => {
+    try {
+      const booking = await Booking.findById(args.bookingId).populate('event');
+      const event = transformEvent(booking.event);
+      await Booking.deleteOne({ _id: args.bookingId });
+      return event;
+    } catch (e) {
+      throw e;
+    }
+  }
+};
